a22fb771f3fbda3b9de1c1e56200ff961508177b
Christopher Schoeder <christopher.schoeder@protonmail.com>
2024-03-23 18:15:42 -0500

    Create Address Model

---
+++ FreightCMS.Locations.Models/Addresses.cs
@@ -0,0 +1,20 @@
+namespace FreightCMS.Locations.Models;
+
+public class AddressModel
+{
+    public required string Id { get; set; }
+    public required string Line1 { get; set; }
+    public string? Line2 { get; set; }
+    public string? Line3 { get; set; }
+    public required string Local { get; set; }
+    public required string Region { get; set; }
+    public required string PostalCode { get; set; }
+    public required string StateOrProvince { get; set; }
+    public required string Country { get; set; }
+    public string? Description { get; set; }
+    public string? Attention { get; set; }
+    /// <summary>
+    /// The type of address. For example, Distribution Center, Store, Convention, School, Office, Port, etc.
+    /// </summary>
+    public required string Type { get; set; }
+}
