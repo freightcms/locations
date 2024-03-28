f06caf4b227c810b4cd0a4ffd922b4b7da40e205
Christopher Schoeder <christopher.schoeder@protonmail.com>
2024-03-27 22:33:22 -0500

    Update model configuration in EF

--- FreightCMS.Locations.EntityFramework/LocationDbContext.cs
+++ FreightCMS.Locations.EntityFramework/LocationDbContext.cs
@@ -4,6 +4,9 @@ using MongoDB.EntityFrameworkCore.Extensions;
 
 namespace FreightCMS.Locations.EntityFramework;
 
+/// <summary>
+/// Creates a new entity framework database context targeting mongodb.
+/// </summary>
 public class LocationDbContext : DbContext
 {
     public LocationDbContext(DbContextOptions<LocationDbContext> options) : base(options)
@@ -16,6 +19,29 @@ public class LocationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AddressModel>().ToCollection("Addresses");
+
+        modelBuilder.Entity<AddressModel>(build => {
+            build.Property(entity => entity.Id).ValueGeneratedOnAdd();
+            build.Property(entity => entity.Line1).IsRequired();
+            build.Property(entity => entity.Local).IsRequired();
+            build.Property(entity => entity.PostalCode).IsRequired();
+            build.Property(entity => entity.Country).IsRequired();
+            build.Property(entity => entity.StateOrProvince).IsRequired();
+            build.Property(entity => entity.Region).IsRequired();
+            build.Property(entity => entity.Type).IsRequired();
+        });
+            
+
         modelBuilder.Entity<CountryModel>().ToCollection("Countries");
+
+        modelBuilder.Entity<CountryModel>(build =>
+        {
+            build.Property(entity => entity.Id).ValueGeneratedOnAdd();
+            build.Property(entity => entity.Name).IsRequired();
+            build.Property(entity => entity.Code).IsRequired();
+            build.Property(entity => entity.Code).HasMaxLength(2);
+            build.Property(entity => entity.Currency).HasMaxLength(3);
+            build.Property(entity => entity.Language).HasMaxLength(5);
+        });
     }
 }
--- FreightCMS.Locations.Models/CountryModel.cs
+++ FreightCMS.Locations.Models/CountryModel.cs
@@ -4,9 +4,18 @@ public class CountryModel
 {
     public required string Id { get; set; }
     public required string Name { get; set; }
+    /// <summary>
+    /// ISO 3166-1 alpha-2 country code
+    /// </summary>
     public required string Code { get; set; }
     public string? Description { get; set; }
+    /// <summary>
+    /// ISO 4217 currency code
+    /// </summary>
     public string? Currency { get; set; }
+    /// <summary>
+    /// ISO 639-1 language code
+    /// </summary>
     public string? Language { get; set; }
     public string? Region { get; set; }
     public string? SubRegion { get; set; }
