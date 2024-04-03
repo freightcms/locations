f0a42676ee71f03e3c5bd8b3a7745cbde5d90097
Christopher Schoeder <christopher.schoeder@protonmail.com>
2024-04-02 21:53:14 -0500

    update locations to use common models

--- models/countries.go
+++ models/countries.go
@@ -1,19 +1,21 @@
 package models
 
+import common "github.com/freightcms/models"
+
 // Country Model meant for providing lookup data for countries. Optionally you extend the address model
 // and add the country Id and override the Address.Country property with a reference to this model.
 type Country struct {
-	Id               string  `json:"id"`
-	Name             string  `json:"name"`
-	Code             string  `json:"code"`
-	Description      *string `json:"description,omitempty"`
-	Currency         *string `json:"currency,omitempty"`
-	Language         *string `json:"language,omitempty"`
-	Region           *string `json:"region,omitempty"`
-	SubRegion        *string `json:"subRegion,omitempty"`
-	Capital          *string `json:"capital,omitempty"`
-	CallingCode      *string `json:"callingCode,omitempty"`
-	Flag             *string `json:"flag,omitempty"`
-	FlagEmoji        *string `json:"flagEmoji,omitempty"`
-	FlagEmojiUnicode *string `json:"flagEmojiUnicode,omitempty"`
+	Id               string           `json:"id"`
+	Name             string           `json:"name"`
+	Code             string           `json:"code"`
+	Description      *string          `json:"description,omitempty"`
+	Currency         *common.Currency `json:"currency,omitempty"`
+	Language         *string          `json:"language,omitempty"`
+	Region           *string          `json:"region,omitempty"`
+	SubRegion        *string          `json:"subRegion,omitempty"`
+	Capital          *string          `json:"capital,omitempty"`
+	CallingCode      *string          `json:"callingCode,omitempty"`
+	Flag             *string          `json:"flag,omitempty"`
+	FlagEmoji        *string          `json:"flagEmoji,omitempty"`
+	FlagEmojiUnicode *string          `json:"flagEmojiUnicode,omitempty"`
 }
--- models/go.mod
+++ models/go.mod
@@ -2,3 +2,4 @@ module github.com/freightcms/locations/models
 
 go 1.21.5
 
+require github.com/freightcms/models v0.0.0-20240403022101-92c2e42c376b // indirect
